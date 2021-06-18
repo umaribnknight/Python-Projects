fc721877fe22cf74aef330da68d1836294d52cc8
umaribnknight <78334001+umaribnknight@users.noreply.github.com>
2021-06-18 13:32:13 -0400

    Create Inheritance.py

---
+++ Inheritance.py
@@ -0,0 +1,24 @@
+#Create two classes that inherit from another class.
+#Ensure each child has at least two of their own attributes.
+
+
+
+
+
+
+class User
+#Define the attribute of parent class
+name = 'No Name Provided'
+email = ' '
+password = '1234abcd'
+account_number = 0
+
+class Driver(User):
+#child class
+    base_pay = 20.00
+    department = 'Delivery'
+
+class Customer(User):
+#child class
+    delivery_address = ' '
+    delivery_list = True
