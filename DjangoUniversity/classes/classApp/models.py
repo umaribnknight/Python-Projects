b7941828ac3f0b37bb9f827e6db18374ac5f7f8a
umaribnknight <78334001+umaribnknight@users.noreply.github.com>
2021-08-05 12:32:50 -0400

    update

---
+++ DjangoUniversity/classes/classApp/models.py
@@ -0,0 +1,18 @@
+# title charfield
+# #course number integer field
+# #intructor charfield
+# #duration float field
+# from django.db import models
+
+from django.db import models
+
+
+class djangoClasses  (models.Model):
+    title = models.CharField(max_length=60)
+    course_number = models.IntegerField(default=0)
+    instructor = models.CharField(max_length=60,blank=True)
+    duration = models.FloatField()
+
+
+
+
---
+++ DjangoUniversity/classes/classApp/views.py
@@ -0,0 +1,6 @@
+from django.http import HttpResponse
+
+def home (request):
+    print(request.user)
+    return HttpResponse("<h1>Welcome user!</h1>")
+
