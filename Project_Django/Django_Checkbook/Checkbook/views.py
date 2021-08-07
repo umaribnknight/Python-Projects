3f09bd85b94710e9177798baa976a56999e3110a
umaribnknight <78334001+umaribnknight@users.noreply.github.com>
2021-08-06 20:24:29 -0400

    update checkbook

--- Project_Django/Django_Checkbook/Checkbook/views.py
+++ Project_Django/Django_Checkbook/Checkbook/views.py
@@ -1,37 +1,30 @@
 from django.shortcuts import render, redirect, get_object_or_404
 from .models import Account, Transaction
 from .forms import AccountForm, TransactionForm
+from django.shortcuts import render, redirect, get_object_or_404
+from .models import Account, Transaction
+from .forms import AccountForm, TransactionForm
 
 def home(request):
     form = TransactionForm(data=request.POST or None)
-    if request.method == 'POST':
-        pk = request.POST['account']
-        return balance(request, pk)
-    content = {'form' : form}
-    return render(request, 'checkbook/index.html', content)
+    content = {'form':form}
+    return render(request, 'checkbook/index.html',content)
 
 def create_account(request):
     form = AccountForm(data=request.POST or None)
     if request.method == 'POST':
         if form.is_valid():
             form.save()
-            pk = request.POST['account']
-            form.save()
-            return balance(request, pk)
+            return redirect('index')
+    content = {'form': form}
+    return render(request, 'checkbook/CreateNewAccount.html', content)
+
+
+
 
 def balance(request, pk):
     account = get_object_or_404(Account, pk=pk)
-    transactions = Transaction.Transactions.filter(account = pk)
-    current_total = account.initial_deposit
-    table_contents = { }
-    for t in transactions:
-        if t.type == 'Deposit':
-            current_total += t.amount
-            table_contents.update({t: current_total})
-        else:
-            current_total += t.amount
-            table_contents.update({t : current_total})
-    content = {'account': account, 'table-contents': table_contents, 'balance' : current_total}
+    content = {'account': account}
     return render(request, 'checkbook/BalanceSheet.html',content)
 
 def transaction(request):
@@ -39,7 +32,9 @@ def transaction(request):
     if request.method == 'POST':
         if form.is_valid():
             form.save()
-            return redirect('index')
+            pk = request.POST['account']
+            form.save()
+            return balance(request, pk)
     content = {'form': form}
     return render(request, 'checkbook/AddTransaction.html', content)
 
